7a414aefbf31cf33944da3976eedc9eb549bfe53
Musab36 <musabhd@gmail.com>
2018-05-13 23:11:53 +0300

    Initial commit

---
+++ app/src/main/java/com/salajim/musab/testing/MainActivity.java
@@ -0,0 +1,58 @@
+package com.salajim.musab.testing;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new AttendenceFragment(), "Attendence");
+        adapter.addFragment(new AssignmentsFragments(), "Assignments");
+        adapter.addFragment(new CourcesFragment(), "Cources");
+        adapter.addFragment(new GradesFragment(), "Grades");
+        adapter.addFragment(new ScheduleFragment(), "Schedule");
+        adapter.addFragment(new BehaviorFragment(), "Behavior");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    // Inflating menus
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
